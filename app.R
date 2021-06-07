d3632e0901572f682f155fe61f03669c137693df
Danielbrey <72413011+Danielbrey@users.noreply.github.com>
2021-06-07 08:52:04 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,29 +1,36 @@
+#The first time you use this program on a computer, you will need to install the packages instead of just calling them, so uncomment out the install.packages lines below.
+#install.packages("shiny")
+#install.packages("tidyverse")
+#install.packages("networkD3")
+#install.packages("igraph")
+
 library(shiny)
 library(tidyverse)
-library(rvest)
-library(shinydashboard)
-library(DT)
 library(networkD3)
 library(igraph)
 
-
+#MisNodes and Mislinks are two data sets that contain data about characters from a book
+#They can be used together to create an SNA graph and collect data
+#MisNodes gives data about each node (character) and MisLinks describes the relationship between nodes
 data(MisLinks)
 data(MisNodes)
 
-#View(Links)
-#View(Nodes)
+#You can use the command "View()" (see below) to look at the datasets (They are commented out at the moment) 
+#View(MisLinks)
+#View(MisNodes)
 
+#We first create nodelist and add each node to the list (for each link, add the two nodes involved)
 nodelist <- c()
-
-#MisLinks$source <- MisLinks$source
-#MisLinks$target <- MisLinks$target
-
 for(i in 1:nrow(MisLinks)){
     nodelist <- append(nodelist, MisLinks$source[i] + 1)
     nodelist <- append(nodelist, MisLinks$target[i] + 1)
 }
+
+#Use SNA measurements to make two vectors (1D arrays) that have values for each node. Will be used for size of nodes in SNA graph
 nodeCloseness <- closeness(graph(nodelist), mode = "all", weights = NA)
 nodeBetweenness <- betweenness(graph(nodelist), directed=T, weights=NA)
+
+#For MisLinks, we created columns to store the groups of each node related to the link
 for(i in 1:nrow(MisLinks)){
     nodeSource <- MisLinks$source[i]
     MisLinks$sourceGroup[i] <- MisNodes$group[nodeSource]
@@ -31,7 +38,7 @@ for(i in 1:nrow(MisLinks)){
     MisLinks$targetGroup[i] <- MisNodes$group[nodeTarget+1]
 }
 
-
+#For each node, we are storing values for different SNA measurements
 for(i in 1:nrow(MisNodes)){
     MisNodes$close[i] <- nodeCloseness[i] * 1000
     MisNodes$between[i] <- nodeBetweenness[i]
@@ -41,17 +48,15 @@ for(i in 1:nrow(MisNodes)){
 } 
 
 
-
+#ui is the frontend part of the app where inputs and outputs are initialized and placed
 ui <- fluidPage(
     
     titlePanel("SNA Graph on Shiny"),
     
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with select inputs to change things about the graph (chooseGroup only works with "full" and group 1) 
     sidebarLayout(
         sidebarPanel(
             inputPanel(
-                #sliderInput("opacity", label = "Opacity",
-                #min = 0.2, max = 0.9, value = 0.5, step = 0.1),
                 selectInput("dataAnalysis", label = "Data Analysis",
                             choices = c("Closeness", "Betweenness")),
                 selectInput("grouping", label = "Select Way of Grouping", choices = c("group", "closeGroup", "betweenGroup")),
@@ -59,7 +64,6 @@ ui <- fluidPage(
             )
         ),
         
-        # Show a plot of the generated distribution
         mainPanel(
             forceNetworkOutput("force"),
             tableOutput("nodes"),
@@ -68,9 +72,6 @@ ui <- fluidPage(
             
         )
     )
-    
-    
-
 )
 
 
